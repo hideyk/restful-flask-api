d9ebdbead16a687b0a916b2d4904cbd37f5a667b
hideyukikanazawa <kanazawahideyuki@gmail.com>
2021-03-14 00:31:13 +0800

    refactor/app.py clean up

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, render_template
-from flask_restful import Resource, Api, reqparse, fields, marshal_with
-import configparser
-import json
+from flask_restful import Resource, Api
 import yaml
 import db.db as db
 import os
@@ -142,4 +140,4 @@ api.add_resource(Transfer, '/transfer/<sender>')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5555)
