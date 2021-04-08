6de3b428f23a7e36c944ab0a2c9f7f19710c4b40
hideyukikanazawa <kanazawahideyuki@gmail.com>
2021-04-08 23:21:33 +0800

    feature/changing port of flask API

--- app.py
+++ app.py
@@ -141,5 +141,5 @@ api.add_resource(Transfer, '/transfer/<sender>')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5555)
+    app.run(debug=True, host='0.0.0.0', port=5000)
     
